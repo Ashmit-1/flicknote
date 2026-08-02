653766f685643526a3945e21868883598cbeeb21
Ashmit Das <dasashmit16@gmail.com>
2026-08-03 00:04:12 +0530

    handle db

---
+++ frontend/src/db.js
@@ -0,0 +1,39 @@
+import localforage from 'localforage'
+
+const store = localforage.createInstance({
+  name: 'quicknotes',
+  storeName: 'quicknotes_data',
+})
+
+const authKey = 'auth'
+const tasksKey = (u) => `tasks:${u}`
+const lastSyncAtKey = (u) => `lastSyncAt:${u}`
+
+export async function getAuth() {
+  return store.getItem(authKey)
+}
+
+export async function setAuth(auth) {
+  return store.setItem(authKey, auth)
+}
+
+export async function clearAuth() {
+  return store.removeItem(authKey)
+}
+
+export async function getTasks(username) {
+  const tasks = await store.getItem(tasksKey(username))
+  return Array.isArray(tasks) ? tasks : []
+}
+
+export async function setTasks(username, tasks) {
+  return store.setItem(tasksKey(username), tasks)
+}
+
+export async function getLastSyncAt(username) {
+  return store.getItem(lastSyncAtKey(username))
+}
+
+export async function setLastSyncAt(username, iso) {
+  return store.setItem(lastSyncAtKey(username), iso)
+}
